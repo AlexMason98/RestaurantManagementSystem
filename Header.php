ea19aeaa11a4fed6dfe4eaa7d39e5651d8e9cd0d
harshdeep <harshdeep3@users.noreply.github.com>
2019-02-05 16:31:28 +0000

    Updates on the pages, where the layout is put in a header.php file and the closing tag are put in a footer,php file. furthermore, the require is now working. Login Page doesn't work but being uplaoded so everyone can access it Co-authored-by: Alex Mason <Alexander. mason.2017@live.rhul.ac.uk>

---
+++ Header.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Oaxaca</title>
+
+	<link rel="icon" type="image/icon" href="images/Favicon.ico">
+	<link rel="stylesheet" type="text/css" href="cssStyleSheets/indexStylePage.css">
+	<link rel="stylesheet" type="text/css" href="cssStyleSheets/loginPage.css">
+	<link rel="stylesheet" type="text/css" href="cssStyleSheets/orderStylePage.css">
+
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body id="body">
+	
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light">
+			<div class="col-lg-2 col-md-2 col-sm-2">
+				<img src="images/Logo.png" id="logo" width ="130" height ="90" ;>
+			</div>
+		</nav>
+	</header>
--- php/Connections/ConnectionCustomer.php
+++ php/Connections/ConnectionCustomer.php
@@ -5,10 +5,10 @@ $username = "root";
 $password = "teamproject5";
 $dbname = "customer";
 // Create/checking connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-mysqli_close($conn);
+
 ?>
