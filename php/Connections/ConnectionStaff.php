5bd24a43145070f9ce366b3e691b05a67cb6db1d
harshdeep <harshdeep3@users.noreply.github.com>
2019-02-11 17:17:09 +0000

    Update ConnectionStaff.php

--- php/Connections/ConnectionStaff.php
+++ php/Connections/ConnectionStaff.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "teamproject5";
+$username = "";
+$password = "";
 $dbname = "staff";
 // Create/checking connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
