fe8b9fb140cd575eaab6d03df0d73c3ba964a9f9
Alex Mason <Alexander.Mason.2017@live.rhul.ac.uk>
2019-03-13 00:06:24 +0000

    Implemented 'Pay After' Option on OrderPage.php
    In this commit I have added a button which gives the customer the flexibility of paying for their order after they've eaten.
    
    This code takes the items they've selected from the TempOrders table and inserts it into the final Orders table once they click "Pay After". There is error checking in place so if they try to place an order with no items in their basket it displays "There are no items in your order". I also check for key violation by checking if an ID in TempOrders exists in Orders from a recently placed order to not only prevent key violations but prevent two of the same item being added to a database table.

--- Code/PHP/CustomerPages/OrderPages/OrderPage.php
+++ Code/PHP/CustomerPages/OrderPages/OrderPage.php
@@ -62,7 +62,8 @@ require '/var/www/html/Alex/PHP/Connections/ConnectionCustomer.php';
 			<div class="row">
 				<div class="col-lg-8 col-md-6 col-sm-4"></div>
 				<div class="col-lg-4 col-md-6 col-sm-8">
-					<input type="submit" class="btn btn-success btn-lg" name="Pay" value="Pay" href="#" />
+					<a class="btn btn-success btn-lg" href="#popupPayBefore">Pay Before</a>
+					<a class="btn btn-success btn-lg" href="#popupPayAfter">Pay After</a>
 					<a class="btn btn-danger btn-lg" href="#popupCancelItems">Cancel Order</a>
 				</div>
 			</div>
@@ -96,6 +97,45 @@ require '/var/www/html/Alex/PHP/Connections/ConnectionCustomer.php';
 							echo('<h5 id="cannotCancel">Can not cancel order. There are no items to remove from your basket</h5>');
 						} else {
 							echo('<h5 id="errorRemoving">ERROR: Could not remove all items from basket</h5>');
+						}
+					?>
+				</div>		
+			</div>
+		</div>
+
+		<div id="popupPayAfter" class="payAfterOverlay">
+			<div class="popup">
+				<a class="close" name="closePopup" href="OrderPage.php">&times;</a>
+				<div class="popupInfo">
+					<?php
+						$sql = "SELECT Orders.ID FROM Orders, TempOrders WHERE Orders.ID = TempOrders.ID";
+						$res = mysqli_query($conn, $sql);
+
+						if (mysqli_num_rows($res) > 0) {
+							echo('<h5>Error Placing Order, Seek Assistance from one of our Waiters</h5>');
+							echo('<h6>You have already ordered an item in this order, please wait until your current order has been fulfilled</h6>');
+
+						} else if ($numberOfRows != 0) {
+									
+							$sql = "INSERT INTO Orders (ID, Item, Quantity, Price, Time) SELECT TempOrders.ID, TempOrders.Item, TempOrders.Quantity, TempOrders.Price, now() FROM TempOrders";
+							if (mysqli_query($conn, $sql)) {
+								$sql = "DELETE FROM TempOrders";
+								if (mysqli_query($conn, $sql)) {
+									echo('<h5 id="receivedOrder">We have received your order!</h5>');
+									echo('<h6 id="paymentLaterText">Call one of our waiters once you have enjoyed your meal to pay</h6>');
+								} else {
+									echo('<h5>Error Placing Order, Seek Assistance from one of our Waiters</h5>');
+									echo('<br>');
+									echo(mysqli_error($conn));
+								}
+							} else {
+								echo('<h5>Error Placing Order, Seek Assistance from one of our Waiters</h5>');
+								echo('<br>');
+								echo(mysqli_error($conn));
+							}
+						} else {
+							echo('<h5>Can not place order!</h5>');
+							echo('<h6>There are no items in your order</h6>');
 						}
 					?>
 				</div>		
