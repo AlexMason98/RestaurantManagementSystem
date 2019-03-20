04fe220b97e745eba2506384932783284499d83a
gbeckenstrater <gjbeckenstrater@gmail.com>
2019-03-20 14:41:22 +0000

    Adding new Categories.php file that uses new way of filtering categories.
    Co-Authored-By: harshdeep <harshdeep3@users.noreply.github.com>

--- Code/PHP/CustomerPages/IndexPage/Categories/Categories.php
+++ Code/PHP/CustomerPages/IndexPage/Categories/Categories.php
@@ -1,29 +1,27 @@
 <div class="card">
-	<div class="card-header" id="headingOne">			
+	<div class="card-header" id="headingOne">
 		<button class="btn btn-lg" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"  id="dropDown">
 			Categories
 		</button>
-		<!-- this is the button drops the list categories down. but at the start the categories button is already opened. -->
 	</div>
 	<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
 		<div class="card-body">
 			<?php
 			require '/var/www/html/Harshdeep/PHP/Connections/ConnectionCustomer.php';
-			//connects to the connection file
 			$sql = "SELECT DISTINCT Category FROM menu";
-			//sql query
 			$res = $conn->query($sql);
-			//gets the results and stores them as the variable 
 			if($res-> num_rows == 0){
 				echo "0 results";
 			}
 			else{
 				while($row = mysqli_fetch_assoc($res)){
-					//
-					echo "<a href = 'Categories/MenuTypes/{$row['Category']}'><br>{$row['Category']}</a>";
+					?>
+
+					<input type="checkbox" name="category[]" value="<?php echo $row['Category'];?>" <?php if(sizeof($_POST['category[]'])>0){ foreach ($_POST['category'] as $key => $value) {if($value == $row['Category']){ echo "checked";}}} ?>> <?php echo $row['Category']; ?> <br>
+					<?php
 				}
+				echo "<br><input type='Submit' value='Submit'>";
 			}
-			//connection closed
 			mysqli_close($conn);
 			?>
 		</div>
