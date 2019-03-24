c976f27fbfde55ec59a31d0c6e9f6f410ca48343
jeevanchakravarthy <46778446+jeevanchakravarthy@users.noreply.github.com>
2019-03-24 23:45:49 +0000

    Added Code which reads user's IP address to differentiate orders
    This code means that on our payment system index page, we can get an order from our database based on the user's IP address. We then have an updated SQL query which selects all items where the stored IP is equal to the user's IP. This gets the items and prices ready to be processed by the payment system and bill the user for them.
    
    Co-Authored-By: alexmason98 <alexmason98@users.noreply.github.com>

--- Code/PHP/CustomerPages/OrderPages/PaymentSystem/index.php
+++ Code/PHP/CustomerPages/OrderPages/PaymentSystem/index.php
@@ -3,6 +3,40 @@ use \PhpPot\Service\StripePayment;
 
 require_once "../../../Header.php";
 require_once "config.php";
+require_once "/var/www/html/Main/PHP/Connections/ConnectionCustomer.php";
+
+
+    // This code gets the user's IP public address and uses it to distinguish the user's order between different systems
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {  // Checks if IP is from shared internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+                                
+    } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { // Checks if IP is passed from a proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                                
+    } else { // Else, get IP address of connecting party 
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+                            
+    $sql = "SELECT * FROM TempOrders WHERE IP = '$ip'";
+    $res = $conn->query($sql);
+    if(!is_null($res)) {
+        $i = 1;
+        while($row = mysqli_fetch_assoc($res)){
+            echo("Row is: ".$i++);
+            $ID = $row['ID'];
+            $Item = $row['Item'];
+            $Quantity = $row['Quantity'];
+            $Price = $row['Price'];
+            $TotalAmount = $Quantity * $Price;
+            echo("ID: ".$ID);
+            echo("Item: ".$Item);
+            echo("Price: ".$Price);
+            echo("Total: ".$TotalAmount);
+        }
+    } else {
+        echo('Error While Parsing Order');
+    }
 
 
 if (!empty($_POST["token"])) {
@@ -15,18 +49,18 @@ if (!empty($_POST["token"])) {
     $databaseConn = new Database();
     
     $amount = $stripeResponse["amount"] /100;
-    
-    $param_type = 'ssdssss';
+
+    $param_type = 'sssssss'; // Indicates type of parameters, 's' denotes String, 'd' denotes Number/Decimal
     $param_value_array = array(
+        $stripeResponse["balance_transaction"],
+        $_POST['name'],
         $_POST['email'],
-        $_POST['item_number'],
         $amount,
         $stripeResponse["currency"],
-        $stripeResponse["balance_transaction"],
         $stripeResponse["status"],
         json_encode($stripeResponse)
     );
-    $query = "INSERT INTO tbl_payment (email, item_number, amount, currency_code, txn_id, payment_status, payment_response) values (?, ?, ?, ?, ?, ?, ?)";
+    $query = "INSERT INTO Payment (TransactionID, Name, Email, TotalAmount, Currency, PaymentStatus, PaymentResponse) values (?, ?, ?, ?, ?, ?, ?)";
     $id = $databaseConn->insert($query, $param_type, $param_value_array);
     
     if ($stripeResponse['amount_refunded'] == 0 && empty($stripeResponse['failure_code']) && $stripeResponse['paid'] == 1 && $stripeResponse['captured'] == 1 && $stripeResponse['status'] == 'succeeded') {
@@ -40,31 +74,27 @@ if (!empty($_POST["token"])) {
     <?php  } ?>
     <div id="error-message"></div>
                 
-            <form id="frmStripePayment" action=""
-                method="post">
+            <form id="frmStripePayment" action="" method="post">
                 <div class="field-row">
-                    <label>Card Holder Name</label> <span
-                        id="card-holder-name-info" class="info"></span><br>
-                    <input type="text" id="name" name="name"
-                        class="demoInputBox">
+                    <label>Card Holder Name</label>
+                    <span id="card-holder-name-info" class="info"></span><br>
+                    <input type="text" id="name" name="name" class="demoInputBox">
                 </div>
                 <div class="field-row">
-                    <label>Email</label> <span id="email-info"
-                        class="info"></span><br> <input type="text"
-                        id="email" name="email" class="demoInputBox">
+                    <label>Email</label> 
+                    <span id="email-info" class="info"></span><br>
+                    <input type="text" id="email" name="email" class="demoInputBox">
                 </div>
                 <div class="field-row">
-                    <label>Card Number</label> <span
-                        id="card-number-info" class="info"></span><br> <input
-                        type="text" id="card-number" name="card-number"
-                        class="demoInputBox">
+                    <label>Card Number</label>
+                    <span id="card-number-info" class="info"></span><br>
+                    <input type="text" id="card-number" name="card-number" class="demoInputBox">
                 </div>
                 <div class="field-row">
                     <div class="contact-row column-right">
-                        <label>Expiry Month / Year</label> <span
-                            id="userEmail-info" class="info"></span><br>
-                        <select name="month" id="month"
-                            class="demoSelectBox">
+                        <label>Expiry Month / Year</label>
+                        <span id="userEmail-info" class="info"></span><br>
+                        <select name="month" id="month" class="demoSelectBox">
                             <option value="01">01</option>
                             <option value="02">02</option>
                             <option value="03">03</option>
@@ -109,11 +139,10 @@ if (!empty($_POST["token"])) {
                         <img alt="loader" src="LoaderIcon.gif">
                     </div>
                 </div>
-                <input type='hidden' name='amount' value='2.99'> <input
-                    type='hidden' name='currency_code' value='GBP'> <input
-                    type='hidden' name='item_name' value='Tortilla Chips With Dips'>
-                <input type='hidden' name='item_number'
-                    value='PHPPOTEG#1'>
+                <input type='hidden' name='amount' value='<?php echo($TotalAmount); ?>'> 
+                <input type='hidden' name='currency_code' value='GBP'>
+                <input type='hidden' name='item_name' value='<?php echo($Item); ?>'>
+                <input type='hidden' name='item_number' value='<?php echo($ID); ?>'>
             </form>
 
     <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
@@ -135,14 +164,14 @@ function cardValidation () {
         valid = false;
     }
     if (email.trim() == "") {
-    	   valid = false;
+        valid = false;
     }
     if (cardNumber.trim() == "") {
-    	   valid = false;
+        valid = false;
     }
 
     if (month.trim() == "") {
-    	    valid = false;
+        valid = false;
     }
     if (year.trim() == "") {
         valid = false;
