43dced2c735af7bc67254bb31fc3648b99f435b8
AlexMason98 <46767100+AlexMason98@users.noreply.github.com>
2019-03-28 18:00:56 +0000

    Added Comments to Config.php
    Added comments explaining what is being defined in the Config.php file

--- Code/PHP/CustomerPages/OrderPages/PaymentSystem/config.php
+++ Code/PHP/CustomerPages/OrderPages/PaymentSystem/config.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+// This defines our secret key linked to our Stripe Dashboard 
 define("STRIPE_SECRET_KEY", "sk_test_uj3JukuwXfFU4xUk8CoUTcem");
+// This defines our publishable key linked to our Stripe Dashboard
 define("STRIPE_PUBLISHABLE_KEY", "pk_test_lqyMXhxxEs5DEXKhWoHfCKm0");
 ?>
