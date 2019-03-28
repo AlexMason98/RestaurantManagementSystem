02573c5182b1ff1ad20e51edf8cee3b8d41c004c
harshdeep <harshdeep3@users.noreply.github.com>
2019-03-28 00:50:20 +0000

    Fixing the kitchen page
    Table fill the whole page, removed some of the code
    
    Co-Authored-By: hanjuwang <hanjuwang@users.noreply.github.com>

--- Code/PHP/StaffPages/KitchenPage/Kitchen.php
+++ Code/PHP/StaffPages/KitchenPage/Kitchen.php
@@ -1,4 +1,3 @@
-
 <!-- Header -->
 <?php
 include_once '../../Header.php';
@@ -7,152 +6,35 @@ include_once '../../Header.php';
 
 
 <section>
-
-	<div class="container-fluid" id="MainContainer">
+	<div class="container-fluid" id="MainContainerKitchen">
 		<div class="row">
-			<div class="col-lg-9 col-md-8 col-sm-7">
-				<div class="currentOrderTable">
-					<table style="width:100%">
-						<colgroup>
-							<col style="width:5%">
-							<col style="width:45%">
-							<col style="width:15%">
-							<col style="width:15%">
-							<col style="width:20%">
-						</colgroup>
+			<div class="col-lg-12 col-md-8 col-sm-7">
+				<table class="table table-striped table-bordered-less header">
+					<thead class="thead-light">
 						<tr>
-							<th>Table</th>
-							<th>Items</th>
-							<th>Time</th>
-							<th>Status</th>
-							<th>Change Status</th>
+							<th style="width:10%">Table</th>
+							<th style="width:50%">Order</th>
+							<th style="width:20%">Time</th>
+							<th style="width:20%">Status</th>
 						</tr>
-						<?php
-						require '../../Connections/ConnectionCustomer.php';
-
-						$sql = "SELECT TableNo, Item, Time,Quantity, Price, Status FROM Orders ORDER BY Time ASC";
-						$res = $conn->query($sql);
-						if($res-> num_rows == 0){
-							echo "0 results";
+					</thead>
+					<?php
+					include_once '../../Connections/ConnectionStaff.php';
+					$sql = "SELECT Time, Items FROM Orders";
+					$res = $conn->query($sql);
+					if($res-> num_rows == 0){
+						echo "0 results";
+					}
+					else{
+						while($row = mysqli_fetch_assoc($res)){
+							echo "<tr><td>{$row['Items']}</td>\n";
+							echo "<td>{$row['Time']}</td></tr>\n";
+							echo "<td>{$row['Status']}</td></tr>\n";
 						}
-						else{
-							$num_rows = 0;
-							while($row = mysqli_fetch_assoc($res)){
-								$num_rows++;
-								$id = $row['ID'];
-								echo "<tr>";
-								echo "<td>{$row['TableNo']}</td><td>{$row['Item']}</td><td>{$row['Time']}</td><td>{$row['Status']}</td><td>";
-
-								$StatusId = "status".$num_rows;
-								$hrefStatusId = "#status".$num_rows;
-								$StatusId = "status".$num_rows."[]";
-								$dropdownChange = "dropdownChange".$num_rows;
-								?>
-
-								<select name="<?php echo $StatusId ?>">
-									<option value="NoStatus">No Status</option>
-									<option value="OrderPlaced">Order Placed</option>
-									<option value="Cooking">Cooking</option>
-									<option value="Cooked">Cooked</option>
-									<option value="Delivered">Delivered</option>
-									<div class="itemBoxes">
-										<?php 
-										$submitButtonID = "submitButton".$num_rows;
-										$hrefSubmitButtonID = "#submitButton".$num_rows;
-										?>
-										<input type="submit" class="btn btn-success" name="<?php echo($submitButtonID); ?>" value="Submit" href="<?php echo($hrefSubmitButtonID); ?>" />
-									</div>
-								</select>
-
-
-								<?php
-								echo "</td>";
-								echo "</tr>";
-							}
-
-							for ($i = 1; $i <= $num_rows; $i++) {
-								$submitButtonID = "submitButton".$i;
-								$StatusID = "status".$i;
-
-								?>
-								<div id="<?php echo($submitButtonID); ?>" class="addItemToCart">
-									<?php
-									if (isset($_POST[$submitButtonID]) && $_POST[$StatusID]) {
-										$ItemRowData =array();
-										$IdRowData = array();
-										$StatusRowData = array();
-										$TableNoRowData = array();
-
-										$AllStatuses = array();
-										array_push($AllStatuses, "NoStatus");
-										array_push($AllStatuses, "OrderPlaced");
-										array_push($AllStatuses, "Cooking");
-										array_push($AllStatuses, "Cooked");
-										array_push($AllStatuses, "Delivered");
-
-										$sql = "SELECT TableNo, ID, Item, Status FROM Orders ORDER BY Time ASC";
-										$res = $conn->query($sql);
-										if ($res -> num_rows == 0) {
-											echo "0 results";
-										}else{
-											while($row = mysqli_fetch_assoc($res)){
-												array_push($IdRowData, $row['ID']);
-												array_push($TableNoRowData, $row['TableNo']);
-												array_push($ItemRowData, $row['Item']);
-												array_push($StatusRowData, $row['Status']);
-											}
-											$GetStatusArray =array();
-											foreach ($_POST[$StatusID] as $value) {
-												array_push($GetStatusArray, $value);
-											}
-											$GetStatus = join("", $GetStatusArray);
-
-											foreach ($StatusRowData as $key => $value) {
-												if ($GetStatus == $value) {
-													echo $value;
-													echo("Status not changed. TRY AGIAN!");
-													break;
-												}else{
-													for ($j=0; $j < $i; $j++) { 
-														switch($GetStatus){
-															case "OrderPlaced":
-															$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
-															break;
-															case "Cooking":
-															$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
-															break;
-															case "Cooked":
-															$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
-															break;
-															case "Delivered":
-															$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
-															break;
-															case "NoStatus":
-															$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
-															break;
-															default:
-															echo"didnt work";
-														}
-													}
-													$res = $conn->query($UpdateSql);
-													if($res === True){
-														echo('<meta http-equiv="refresh" content="0">');
-													} else{
-														echo "Error updating record! Try again.";
-													}
-												}
-											}
-										}
-									}
-									?>
-								</div>
-								<?php
-							}
-						} 
-						mysqli_close($conn);
-						?>
-					</table>
-				</div>
+					}
+					mysqli_close($conn);
+					?>
+				</table>
 			</div>
 		</div>
 	</div>
@@ -161,6 +43,7 @@ include_once '../../Header.php';
 
 <!-- Footer -->
 <?php
-include_once '../footer.php';
+ // this holds the closing body tag and html tag
+include_once '../../footer.php';
 ?>
-<!-- Footer -->
+ <!-- Footer -->
