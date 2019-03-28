d2d8f2197dbea4738b5f16f72d0779cdb1acc13c
rashid341 <46781386+rashid341@users.noreply.github.com>
2019-03-28 13:19:18 +0000

    populates table using sql
    Co-Authored-By: harshdeep <harshdeep3@users.noreply.github.com>

--- Code/PHP/StaffPages/KitchenPage/Kitchen.php
+++ Code/PHP/StaffPages/KitchenPage/Kitchen.php
@@ -4,7 +4,6 @@ include_once '../../Header.php';
 ?>
 <!-- Header -->
 
-
 <section>
 	<div class="container-fluid" id="MainContainerKitchen">
 		<div class="row">
@@ -27,20 +26,69 @@ include_once '../../Header.php';
 						echo "0 results";
 					}
 					else{
+						$num_rows = 0;
 						while($row = mysqli_fetch_assoc($res)){
+							$num_rows++;
 							echo "<tr><td>{$row['TableNo']}</td>";
 							echo "<td>{$row['Item']}</td>";
 							echo "<td>{$row['Time']}</td>";
 							echo "<td>{$row['Quantity']}</td>";
+							echo "<div>";
+							$cookingButton = "cooking".$num_rows;
+							$hrefcookingButton = "#cooking".$num_rows;
+							$cookedButton = "cooked".$num_rows;
+							$hrefcookingButton = "#cooked".$num_rows;
 							?>
 							<td>
-								<input type="Submit" name="Cooking" value="Cooking">
-								<input type="Submit" name="Cooked" value="Cooked">
+								<input type="Submit" name="<?php echo $cookingButton;?>" value="Cooking">
+								<input type="Submit" name="<?php echo $cookedButton;?>" value="Cooked">
 							</td>
 							<?php
+							echo "<div>";							
 						}
+						for ($i=1; $i <= $num_rows; $i++) { 
+							$CookingBut = "cooking".$i;
+							$CookedBut = "cooked".$i;
+
+							if(isset($_POST['CookedBut']) or isset($_POST['CookingBut'])) {
+
+								echo "here";
+							// 	$ItemRowData =array();
+							// 	$IdRowData = array();
+							// 	$TableNoRowData = array();
+
+							// 	$sql = "SELECT TableNo, Item, Time, Quantity, Status FROM Orders ORDER BY Time ASC";
+							// 	$res = $conn->query($sql);
+							// 	if ($res -> num_rows == 0) {
+							// 		echo "0 results";
+							// 	}else{
+							// 		while ($row = mysqli_fetch_assoc($res)) {
+							// 			array_push($IdRowData, $row['ID']);
+							// 			array_push($TableNoRowData, $row['TableNo']);
+							// 			array_push($ItemRowData, $row['Item']);
+							// 		}
+
+							// 		for ($j=0; $j < $i; $j++) { 
+							// 			$IdRowData[$j];
+							// 			if($_POST[$CookedBut]){
+							// 				$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
+							// 			}elseif($_POST[$CookingBut]){
+							// 				$UpdateSql = "UPDATE  Orders SET Status = '$GetStatus' WHERE Item='$ItemRowData[$j]' AND TableNo='$TableNoRowData[$j]' AND ID ='$IdRowData[$j]'";
+							// 			}
+							// 		}
+							// 		echo $UpdateSql;
+							// 	// $res = $conn->query($UpdateSql);
+							// 	// if($res === True){
+							// 	// 	echo('<meta http-equiv="refresh" content="0">');
+							// 	// } else{
+							// 	// 	echo "Error updating record! Try again.";
+							// 	// }
+
+							// 	}
+							}
+						}
+						mysqli_close($conn);		
 					}
-					mysqli_close($conn);
 					?>
 				</table>
 			</div>
