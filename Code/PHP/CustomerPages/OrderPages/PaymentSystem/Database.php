2dd1feb59af0327dbee76a1517a123195528be33
AlexMason98 <46767100+AlexMason98@users.noreply.github.com>
2019-03-28 17:55:52 +0000

    Added Comments to Database.php
    This explains how each segment of code works in this file

--- Code/PHP/CustomerPages/OrderPages/PaymentSystem/Database.php
+++ Code/PHP/CustomerPages/OrderPages/PaymentSystem/Database.php
@@ -1,20 +1,24 @@
 <?php
 class Database {
+    // This sets up the database connection with our credentials
 	private $host = "localhost";
 	private $user = "root";
 	private $password = "teamproject5";
 	private $database = "customer";
 	private $conn;
-	
+
+    // Constructs the file by calling the connectDB method to initialise connection
     function __construct() {
         $this->conn = $this->connectDB();
 	}	
 	
+    // Connect DB method to connect to the database, ready to run queries
 	function connectDB() {
 		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
 		return $conn;
 	}
 	
+    // Runs base queries using the database connection
     function runBaseQuery($query) {
         $result = $this->conn->query($query);	
         if ($result->num_rows > 0) {
@@ -26,7 +30,7 @@ class Database {
     }
     
     
-    
+    // Runs queries in the database using the database connection
     function runQuery($query, $param_type, $param_value_array) {
         $sql = $this->conn->prepare($query);
         $this->bindQueryParams($sql, $param_type, $param_value_array);
@@ -44,6 +48,7 @@ class Database {
         }
     }
     
+    // Bind paramters received from the query to other bits of data
     function bindQueryParams($sql, $param_type, $param_value_array) {
         $param_value_reference[] = & $param_type;
         for($i=0; $i<count($param_value_array); $i++) {
@@ -55,12 +60,14 @@ class Database {
         ), $param_value_reference);
     }
     
+    // Function to insert data into the database
     function insert($query, $param_type, $param_value_array) {
         $sql = $this->conn->prepare($query);
         $this->bindQueryParams($sql, $param_type, $param_value_array);
         $sql->execute();
     }
     
+    // Function to update data into the database
     function update($query, $param_type, $param_value_array) {
         $sql = $this->conn->prepare($query);
         $this->bindQueryParams($sql, $param_type, $param_value_array);
