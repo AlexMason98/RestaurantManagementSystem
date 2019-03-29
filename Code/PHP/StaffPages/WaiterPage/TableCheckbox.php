e4f002402db6ef1bae84cf91a1f9c8e684f503df
harshdeep <harshdeep3@users.noreply.github.com>
2019-03-29 01:39:40 +0000

    Fixing CSS
    Made the table assignment page look better.
    
    Co-Authored-By: gracebrumder <gracebrumder@users.noreply.github.com>
    Co-Authored-By: jeevanchakravarthy <jeevanchakravarthy@users.noreply.github.com>

--- Code/PHP/StaffPages/WaiterPage/TableCheckbox.php
+++ Code/PHP/StaffPages/WaiterPage/TableCheckbox.php
@@ -1,78 +1,76 @@
-<div class="col-lg-12">
-	<div class="row">
-		<div class="col-lg-4"></div>
-		<div class="col-lg-4" >
-			<img src="TablePlan.png" id="TablePlan" width ="600" height ="300">
-		</div>
-		<div class="col-lg-4"></div>
-	</div>
-	<div class="row" id="ChangeAssistance">
-		<div class="col-lg-1"></div>
-		<div class="col-lg-5">
-			<h1>Table Plan</h1>
-			<p>Click to claim a table</p>
-			<p>Enter Table number</p>
-			<input type="number" name="TableNumber" placeholder="Table Number">
-			<?php
-			require '/var/www/html/Main/PHP/Connections/ConnectionStaff.php';
-			//connects to the connection file
-			$sql = "SELECT * FROM Logins";
-			//sql query
-			$res = $conn->query($sql);
-			//gets the results and stores them as the variable 
-			if($res-> num_rows == 0){
-				echo "0 results";
-			}
-			else{
-				while($row = mysqli_fetch_assoc($res)){
-					?>
-					<option>
-						<input type="checkbox" name="WaiterAssignment[]" value="<?php echo $row['Fullname']; ?>"><?php echo " ".$row['Fullname']; ?><br>
-						<!-- This is the checkbox for which names of the Waiter. So the can be assigned to a table. -->
-					</option>
-					<?php
-				}
-				echo "<option>";
-				echo "<input type='checkbox' name='WaiterAssignment[]' value='Not Assigned'; > Not Assigned<br>";
-				echo "</option>";
-				echo "<br><input type='Submit'  value='Submit'>";
+<div class="container-fluid">
+	<div class="col-lg-12">
+		<div class="row" >
+			<div class="col-lg-4" id="ChangeAssistance">
+				<h1>Table Plan</h1>
+				<p>Click to claim a table</p>
+				<p><br>Enter Table number</p>
 
-			}
-			?>
-		</div>
-		<div class="col-lg-5" id="TATable">
-			<table style="width: 100%">
-				<tr>
-					<th style="width: 16%; font-size: 25px;">Table</th>
-					<th style="width: 30%; font-size: 25px;">Time</th>
-					<th style="width: 27%; font-size: 25px;">Waiter Name</th>
-					<th style="width: 27%; font-size: 25px;">Status</th>
-					<!-- The heading for the table -->
-				</tr>
+				<input type="number" name="TableNumber" placeholder="Table Number"><br>
 				<?php
-				$sql = "SELECT * FROM TableAssistance";
+				require '/var/www/html/Main/PHP/Connections/ConnectionStaff.php';
+			//connects to the connection file
+				$sql = "SELECT * FROM Logins";
+			//sql query
 				$res = $conn->query($sql);
+			//gets the results and stores them as the variable 
 				if($res-> num_rows == 0){
 					echo "0 results";
-				}else{
-					while ($row = mysqli_fetch_assoc($res)) {
-						echo "<tr><td>{$row['TableID']}</td>";
-						echo "<td>{$row['Time']}</td>";
-						echo "<td>{$row['WaiterName']}</td>";
-						echo "<td>{$row['Status']}</td></tr>";
+				}
+				else{
+					while($row = mysqli_fetch_assoc($res)){
+						?>
+
+							<input type="checkbox" name="WaiterAssignment[]" value="<?php echo $row['Fullname']; ?>"><?php echo " ".$row['Fullname']; ?><br>
+							<!-- This is the checkbox for which names of the Waiter. So the can be assigned to a table. -->
+						<?php
 					}
+
+					echo "<input type='checkbox' name='WaiterAssignment[]' value='Not Assigned'; > Not Assigned<br>";
+
+					echo "<br><input type='Submit'  value='Submit'>";
+
 				}
 				?>
-			</table>
-		</div>
-		<div class="col-lg-1"></div>
-	</div>
+			</div>
 
+			<div class="col-lg-7">
+				<div class="row">
+					<img src="TablePlan.png" id="TablePlan" width ="600" height ="300">
+				</div>
+				<div class="row"></div>
+				<div class="row" id="TATable">
+					<table style="width: 100%">
+						<tr>
+							<th style="width: 16%; font-size: 25px;">Table</th>
+							<th style="width: 30%; font-size: 25px;">Time</th>
+							<th style="width: 27%; font-size: 25px;">Waiter Name</th>
+							<th style="width: 25%; font-size: 25px;">Status</th>
+							<!-- The heading for the table -->
+						</tr>
+						<?php
+						$sql = "SELECT * FROM TableAssistance";
+						$res = $conn->query($sql);
+						if($res-> num_rows == 0){
+							echo "0 results";
+						}else{
+							while ($row = mysqli_fetch_assoc($res)) {
+								echo "<tr><td>{$row['TableID']}</td>";
+								echo "<td>{$row['Time']}</td>";
+								echo "<td>{$row['WaiterName']}</td>";
+								echo "<td>{$row['Status']}</td></tr>";
+							}
+						}
+						?>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 
 <?php
-
 
 if(!empty($_POST['TableNumber']) && !empty($_POST['WaiterAssignment'])){
 	$TableNumberArray = $_POST['TableNumber'];
